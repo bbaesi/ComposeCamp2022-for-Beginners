7ed7060e9134f8c8ff9cbebbde9027d630999796
bbaesi <devbsh013@gmail.com>
2022-12-09 17:52:32 +0900

    [Art Space 프로토 타입 완성]

--- Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -6,15 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +31,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                // A surface container using the 'background' color from the theme
                 Surface()
                 {
                     ArtSpaceScreen()
@@ -38,19 +42,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceScreen() {
-    var imageResource by remember { mutableStateOf(R.drawable.lemon_tree)} // 상태 변경 추적 및 저장
+    val imageList = arrayListOf(R.drawable.lemon_tree, R.drawable.lemon_squeeze,
+        R.drawable.lemon_drink, R.drawable.lemon_restart)
+    val nameList = arrayListOf(R.string.name1,R.string.name2,R.string.name3,R.string.name4)
+    val titleList = arrayListOf(R.string.content_Lemontree,R.string.content_Lemon,
+        R.string.content_Glassoflemonade, R.string.content_Emptyglass)
+    val yearlist = arrayListOf("(2019)", "(2020)", "(2021)", "(2022)")
+
+    var imageResource by remember { mutableStateOf(R.drawable.lemon_tree)}
+    var nameResource by remember { mutableStateOf(R.string.name1)}
+    var titleResource by remember { mutableStateOf(R.string.content_Lemontree)}
+    var count by remember { mutableStateOf(0)}
 
     Column(
         modifier = Modifier
             .padding(10.dp)
             .fillMaxSize()
             .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center
     )
     {
         Column(
             modifier = Modifier
-                .padding(10.dp),
+                .padding(10.dp)
+                .fillMaxWidth()
+                .height(400.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             ArtImageField(imageResource)
         }
@@ -58,45 +75,96 @@ fun ArtSpaceScreen() {
         Column(
             modifier = Modifier
                 .padding(10.dp)
-                .fillMaxSize(),
+                .fillMaxWidth()
+                .shadow(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
         ) {
-            Text(
-                text = "ArtWork Title",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-
-            Text(
-                text = "ArtWork Title",
-                fontSize = 16.sp,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )        
+            ArtTextField(titleResource, 24)
+            ArtTextField(nameResource, 16, FontWeight.Bold, year = yearlist[count])
+        }
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxSize(),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Spacer(Modifier.weight(1F))
+            Button(
+                onClick = {
+                    if(count==0) count = 3
+                    else count --
+                    imageResource = imageList[count]
+                    titleResource = titleList[count]
+                    nameResource = nameList[count] },
+                modifier = Modifier
+                    .width(120.dp)
+            ) {
+                Text(text = "Previous")
+            }
+            Spacer(Modifier.weight(1F))
+            Button(
+                onClick = {
+                    if(count==3) count = 0
+                    else count++
+                    imageResource = imageList[count]
+                    titleResource = titleList[count]
+                    nameResource = nameList[count] },
+                modifier = Modifier
+                    .width(120.dp)
+            ) {
+                Text(text = "Next")
+            }
+            Spacer(Modifier.weight(1F))
         }
     }
 }
 
 @Composable
-fun ArtButtonField(
+fun ArtTextField(
+    @StringRes txt : Int,
+    size : Int,
+    fontWeight  : FontWeight = FontWeight.Normal,
+    year : String = ""
 ){
-    Column(
-
-    ) {
-        Button(onClick = { /*TODO*/ }) {
-            Modifier
+    Row(){
+        Text(
+            text = stringResource(id = txt),
+            fontSize = size.sp,
+            fontWeight = fontWeight
+        )
+        if(year.isNotEmpty()){
+            Text(
+                text = year,
+                fontSize = size.sp,
+                fontWeight = FontWeight.Normal
+            )
         }
     }
 }
-
 
 @Composable
 fun ArtImageField(
     @DrawableRes imageResource: Int
 ){
-    Image(
-        painter = painterResource(id = imageResource),
-        contentDescription = null
-    )
+    Box(
+        modifier = Modifier
+            .shadow(1.dp)
+            .border(
+                width = 3.dp,
+                color = Color.DarkGray,
+                shape = RectangleShape
+            )
+
+    ){
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .padding(20.dp)
+        )
+    }
 }
 
 
