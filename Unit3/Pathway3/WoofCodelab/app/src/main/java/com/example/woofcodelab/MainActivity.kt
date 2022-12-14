5de95f0b97909833f9d0d58620fccb8197125f94
bbaesi <devbsh013@gmail.com>
2022-12-14 16:52:11 +0900

    [Material 코드랩 완료 (WoofCodelab)]

--- Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
+++ Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
@@ -15,12 +15,14 @@
  */
 package com.example.woofcodelab
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -28,9 +30,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,83 +60,134 @@ class MainActivity : ComponentActivity() {
 }
 
 /**
- * Composable that displays an app bar and a list of dogs.
+ * * 앱 표시줄과 강아지 목록을 표시하는 Composable.
  */
 @Composable
 fun WoofApp() {
-    LazyColumn {
-        items(dogs) {
-            DogItem(dog = it)
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier.background(MaterialTheme.colors.background)
+                .padding(it) // UnusedMaterialScaffoldPaddingParameter 방지
+        ) {
+            items(dogs) {
+                DogItem(dog = it)
+            }
         }
     }
 }
 
 /**
- * Composable that displays a list item containing a dog icon and their information.
+ * 강아지 아이콘과 해당 정보가 포함된 목록 항목을 표시하는 Composable.
  *
- * @param dog contains the data that populates the list item
- * @param modifier modifiers to set to this composable
+ * @param dog 리스트 항목을 채우는 데이터
+ * @param modifier 이 composable 수정자
  */
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = 4.dp
     ) {
-        DogIcon(dog.imageResourceId)
-        DogInformation(dog.name, dog.age)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            DogIcon(dog.imageResourceId)
+            DogInformation(dog.name, dog.age)
+        }
     }
 }
 
 /**
- * Composable that displays a photo of a dog.
+ * 강아지 사진을 표시하는 Composable.
  *
- * @param dogIcon is the resource ID for the image of the dog
- * @param modifier modifiers to set to this composable
+ * @param dogIcon 는 강아지 이미지의 리소스 ID
+ * @param modifier 이 composable 수정자
  */
 @Composable
 fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
+
     Image(
         modifier = modifier
             .size(64.dp)
-            .padding(8.dp),
+            .padding(8.dp)
+            .clip(RoundedCornerShape(50)),
+        contentScale = ContentScale.Crop,
         painter = painterResource(dogIcon),
         /*
-         * Content Description is not needed here - image is decorative, and setting a null content
-         * description allows accessibility services to skip this element during navigation.
+         * 콘텐츠 설명 x, 이미지는 장식용이며 null 사용
+         * 설명은 접근성 서비스가 탐색 중에 이 요소를 건너 뛸 수 있도록 합니다.
          */
         contentDescription = null
     )
 }
 
 /**
- * Composable that displays a dog's name and age.
+ * 강아지 이름과 나이 표시하는 Composable
  *
- * @param dogName is the resource ID for the string of the dog's name
- * @param dogAge is the Int that represents the dog's age
- * @param modifier modifiers to set to this composable
+ * @param dogName 는 개의 이름 문자열에 대한 리소스 id
+ * @param dogAge 는 개의 나이를 나타내는 int
+ * @param modifier 수정자
  */
 @Composable
 fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Modifier) {
     Column {
         Text(
             text = stringResource(dogName),
+            style = MaterialTheme.typography.h2,
             modifier = modifier.padding(top = 8.dp)
         )
         Text(
-            text = stringResource(R.string.years_old, dogAge)
+            text = stringResource(R.string.years_old, dogAge),
+            style = MaterialTheme.typography.body1
+
         )
     }
 }
 
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .background(color = MaterialTheme.colors.primary)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp),
+            painter = painterResource(R.drawable.ic_woof_logo),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1
+        )
+    }
+}
+
+
 /**
- * Composable that displays what the UI of the app looks like in light theme in the design tab.
+ * 밝은 테마에서 ui가 어떻게 보이는지 테스트하는 Composable
  */
 @Preview
 @Composable
 fun WoofPreview() {
     WoofTheme(darkTheme = false) {
+        WoofApp()
+    }
+}
+
+@Preview
+@Composable
+fun DarkThemePreview() {
+    WoofTheme(darkTheme = true) {
         WoofApp()
     }
 }
